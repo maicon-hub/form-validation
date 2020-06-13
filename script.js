f3cc3a88507962360cf100970ce8d28ef49d0cb8
maicon <maiconandradexd@gmail.com>
2020-06-13 15:28:39 -0300

    form validation with HTML and JS

---
+++ script.js
@@ -0,0 +1,90 @@
+const fields = document.querySelectorAll("[required]")
+
+function validateField(field) {
+
+  function verifyErrors() {
+    let foundError = false;
+
+    // verify if error exists
+    for (let error in field.validity) {
+
+      if (field.validity[error] && !field.validity.valid) {
+        foundError = error
+      }
+
+    }
+    return foundError;
+  }
+
+  function customMessage(typeError) {
+    const messages = {
+      text: {
+        valueMissing: "Por favor, preencha este campo"
+      },
+      email: {
+        valueMissing: "Email obrigatorio",
+        typeMismatch: "Insira um email valido"
+      }
+    }
+
+    return messages[field.type][typeError]
+  }
+
+  function setCustomMessage(message) {
+    const spanError = field.parentNode.querySelector("span.error")
+
+    if (message){
+      
+      spanError.classList.add("active")
+      spanError.innerHTML = message
+  
+    } else {
+      
+      spanError.classList.remove("active")
+      spanError.innerHTML = ""
+    }
+
+  }
+
+  return function() {
+
+    const error = verifyErrors();
+
+    if (error){
+      const message = customMessage(error);
+
+      field.style.borderColor = "red"
+      setCustomMessage(message)
+    } else {
+      field.style.borderColor = "green"
+      setCustomMessage()
+    }
+  }
+}
+
+function customValidation(event) {
+  
+  const field = event.target
+  const validation = validateField(field)
+  
+  validation()
+}
+
+for (field of fields) {
+  field.addEventListener("invalid", event => {
+    // clear the bubble
+    event.preventDefault()
+
+    customValidation(event)
+  })
+  field.addEventListener("blur", customValidation)
+}
+
+document.querySelector("form")
+  .addEventListener("submit", event => {
+    console.log("enviar o formulario")
+
+    // preventes form submission
+    event.preventDefault()
+  }
+)
